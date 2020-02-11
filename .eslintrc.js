bf252e73afd946a346e4e6fb823356f2040ef040
koreanwglasses <koreanwglasses@gmail.com>
2020-02-11 17:33:37 -0500

    initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error"
+  }
+};
