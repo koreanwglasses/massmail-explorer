388b49ea0cf4304b25eae7ae54ac9977f140c6c7
koreanwglasses <koreanwglasses@gmail.com>
2021-02-22 12:19:35 -0600

    mock data

--- data/process_emails.py
+++ data/process_emails.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 # useful for ensuring paths are consistent. Use with os.path.join(SCRIPT_DIR, ...)
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -7,11 +8,11 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 if __name__ == "__main__":
   # TODO: load and process massmails/emails
 
+  clusters = [ { "id": 0, "label": "apple" }, { "id": 1, "label": "banana" }, { "id": 2, "label": "carrot" } ]
+
   results = { 
     "emails": [
-      { "embedding": { "x": 0.6, "y": 0.1 } },
-      { "embedding": { "x": 0.8, "y": 0.2 } },
-      { "embedding": { "x": 0.2, "y": 0.5 } }
+      { "embedding": { "x": random.random(), "y": random.random() }, "clusterId": random.choice(clusters)["id"] } for _ in range(10)
     ] 
   }; 
 
